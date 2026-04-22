594cfab0d5aae65e94ee82704be9b9fd6d1defaa
sdrindahl <sdrindahl@gmail.com>
2026-04-22 00:06:59 -0500

    added use client

--- app/course-nines/page.tsx
+++ app/course-nines/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import PageWrapper from "@/components/PageWrapper";
